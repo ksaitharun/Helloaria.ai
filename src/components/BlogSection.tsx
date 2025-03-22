39768aba55bbae108f3550fbe4d668e2d2251730
ksaitharun <34804944+ksaitharun@users.noreply.github.com>
2025-03-22 17:20:48 +0530

    Update BlogSection.tsx

--- src/components/BlogSection.tsx
+++ src/components/BlogSection.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { ArrowRight } from 'lucide-react';
 import Link from 'next/link';
-import { blogPosts } from '@/lib/blog-data';
+import blogPosts from '@/lib/blog-data';
 
 const BlogSection = () => {
   return (
